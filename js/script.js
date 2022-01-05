08acb789b3cdc33c9822aad7268f61f9d5a54db3
Nana <nanalit89@gmail.com>
2022-01-05 10:40:05 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Nana';
+alert(`Привет ${user}!`);
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked || !isClose);
